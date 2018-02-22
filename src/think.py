78c02b2b37ae482f53ea87e171a3f8cdb8112da9
陈辉 <chenhui2547@qianmi.com>
2018-02-22 09:23:56 +0800

    春节

--- src/think.py
+++ src/think.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    print 'think.py'
+    print '--- think.py ---'
 
 # 代码1
 # def print_lyrics():
@@ -25,6 +25,7 @@ if __name__ == '__main__':
 # print("打印字符串切片:{0}".format(samples[:]))
 
 # 代码4
+# print的新用法，括号中说明多个变量用逗号分开
 # families = ["chenhui","chenyanqiu","xuzhongxia"]
 # for fam in families:
 #     print fam
@@ -32,7 +33,7 @@ if __name__ == '__main__':
 # print('chenhui in Famliy: {0}'.format('Chenhui' in families))
 
 # for i in range(len(families)):
-#     print 'Member{0}: {1} '.format(i,families[i])
+#     print 'Member_{0}: {1} '.format(i,families[i])
 
 # def nested_sum(raw_list):
 #     total = 0
@@ -114,6 +115,41 @@ if __name__ == '__main__':
 # print file_hash.hexdigest()
 
 # 代码8
-from student import Student
-s = Student('chenhui',100)
-s.print_score()
+# from student import Student
+# s = Student('chenhui',100)
+# s.print_score()
+
+
+## 代码9
+# for a in [3,4.4,"life"]:
+#     print '打印序列元素: {0}'.format(a)
+
+## 代码10
+# 总价100万的房子，全额贷款买房，前四年利率有折扣，分别是1%、2%、3%、3.5%
+# 其余年份里房贷利率都是5%，逐年还款，每年最多偿还3万元。
+# 问: 完全还清房贷需要多少年？
+
+# year = 0
+# total_amount = 500000.0
+# interest_rates = (0.01,0.02,0.03,0.035)
+# repay = 30000.0
+# while total_amount > 0 :
+#     year = year + 1
+#     # print '第{0}年偿还贷款...'.format(year)
+#     if year <= 4 :
+#         interest = interest_rates[year-1]
+#     else:
+#         interest = 0.05
+#     total_amount = total_amount * (interest + 1) - repay
+#     print '剩余金额%.2f' % total_amount
+
+# print '恭喜您，通过{0}年终于还清了房贷'.format(year)
+
+## 代码11
+## 函数定义: 形参
+## 函数调用: 实参
+# def print_hello():
+#     """ print hello world """
+#     print 'Hello World'
+# help(print_hello)
+# print_hello()
